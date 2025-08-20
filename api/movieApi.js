8890faceff2d1e8b0205bbb731b309c7536f8b08
Dennis <dennis-farris@hotmail.com>
2025-08-20 11:47:15 -0400

    movie search controller finished

--- api/movieApi.js
+++ api/movieApi.js
@@ -1,10 +1,8 @@
-require("dotenv").config();
-
 const axios = require("axios");
 
 
 const movieApi = axios.create({
-    baseURL: `http://img.omdbapi.com/?apikey=${OMDB_API_KEY}&`
+    baseURL: `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&`
 })
 
 module.exports = movieApi;
--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,9 +1,27 @@
-require("dotenv").config();
+const movieApi = require("../api/movieApi");
 
-const searchMovies = (req, res) => {
+const searchMovies = async (req, res) => {
     const title = req.query.title;
 
     if (!title)
         res.status(404).json({"error": "Title query parameter is required"});
-        return;
+    else {
+        try {
+            const url = `s=${title}`
+            const response = await movieApi.get(url);
+
+            res.json(response.data);
+        }
+        catch (error) {
+            if(error.response) {console.error("API Error:", error.response.status, error.response.message);
+            res.status(error.response.status).json({message: "Error fetching data from external api"})
+            }
+            else {
+                console.error("Network Error:", error.message);
+                res.status(500).json({message: "A network error has occurred"})
+            }
+        }
+    }
 }
+
+module.exports = searchMovies;
