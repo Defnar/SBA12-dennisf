bc4cd6d0210d465a8f26b9c99630d7581a688b0a
Dennis <dennis-farris@hotmail.com>
2025-08-20 12:00:34 -0400

    refactored into a separate fetch function, and created a function to grab movie details

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,27 +1,45 @@
 const movieApi = require("../api/movieApi");
 
-const searchMovies = async (req, res) => {
-    const title = req.query.title;
-
-    if (!title)
-        res.status(404).json({"error": "Title query parameter is required"});
-    else {
-        try {
-            const url = `?apikey=${OMDB_API_KEY}&s=${title}`
-            const response = await movieApi.get(url);
-
-            res.json(response.data);
-        }
-        catch (error) {
-            if(error.response) {console.error("API Error:", error.response.status, error.response.message);
-            res.status(error.response.status).json({message: "Error fetching data from external api"})
-            }
-            else {
-                console.error("Network Error:", error.message);
-                res.status(500).json({message: "A network error has occurred"})
-            }
-        }
+const fetchData = async (url, req, res) => {
+  try {
+    const response = await movieApi.get(url);
+    res.json(response.data);
+  } catch (error) {
+    if (error.response) {
+      console.error(
+        "API Error:",
+        error.response.status,
+        error.response.message
+      );
+      res
+        .status(error.response.status)
+        .json({ message: "Error fetching data from external api" });
+    } else {
+      console.error("Network Error:", error.message);
+      res.status(500).json({ message: "A network error has occurred" });
     }
-}
+  }
+};
 
-module.exports = searchMovies;
+const searchMovies = async (req, res) => {
+  const title = req.query.title;
+
+  if (!title)
+    res.status(404).json({ error: "Title query parameter is required" });
+  else {
+    const url = `?apikey=${OMDB_API_KEY}&s=${title}`;
+    fetchData(url, req, res);
+  }
+};
+
+const getMovieDetails = async (req, res) => {
+  const id = req.params.id;
+  if (!id || id === "") {
+    res.status(404).json({ error: "id is required" });
+  } else {
+    const url = `?apikey=${OMDB_API_KEY}&i=${id}`;
+    fetchData(url, req, res);
+  }
+};
+
+module.exports = { searchMovies, getMovieDetails };
